09db096fe9b34a8b16f3667005fadfbfb42cc254
stevie35 <stevie.leveque@outlook.com>
2020-09-14 16:26:51 +0200

    Add patch and delete route for connected user

--- src/controllers/profiles.ts
+++ src/controllers/profiles.ts
@@ -7,3 +7,8 @@ export function getProfile(profileId: string): Promise<IProfile | null>{
 export function getAllProfiles(): Promise<IProfile[]>{
   return Profile.find({}).then(profiles => profiles);
 }
+
+export function updateProfile(profile: IProfile, email: string, firstname: string, lastname: string, password?: string): Promise<IProfile | null>{
+  if(password) profile.setPassword(password);
+  return Profile.findByIdAndUpdate(profile._id, {email, firstname, lastname}).then(profile => profile); 
+  } 
--- src/routes/profileRoute.ts
+++ src/routes/profileRoute.ts
@@ -1,7 +1,7 @@
-import { Request, Response, Router } from "express";
+import { request, Request, Response, Router } from "express";
 import { IProfile, Profile } from "../models/profiles";
 import { authenticationRequired } from "../middlewares/authenticationRequired";
-import { getAllProfiles, getProfile } from '../controllers/profiles';
+import { getAllProfiles, getProfile, updateProfile } from '../controllers/profiles';
 
 const router = Router();
 
@@ -9,6 +9,31 @@ router.get("/me", authenticationRequired, (request: Request, response: Response)
   if(!request.user) { return response.status(401).send() }
   return response.json((request.user as IProfile).getSafeProfile());
 });
+
+router.patch("/", authenticationRequired, (request: Request, response: Response) => {
+  if(!request.user) { return response.status(401).send() }
+  const { email, firstname, lastname, password } = request.body;
+
+  updateProfile(request.user as IProfile, email, firstname, lastname, password)
+    .then(profile => {
+      if(!profile) return response.status(404).send("Profile not found");
+      return response.status(200).send(profile.getSafeProfile());
+    })
+    .catch(error => {
+      console.error(error);
+      return response.status(500).send();
+    });
+})
+
+router.delete('/', authenticationRequired, (req: Request, res: Response) => {
+  if(!request.user) { return res.status(401).send() }
+  (request.user as IProfile).deleteOne()
+    .then(_profile => res.status(200).send('Utilisateur supprimé'))
+    .catch(error => {
+      console.error(error);
+      return res.status(500).send();
+    });
+})
 
 router.post("/", (req: Request, res: Response) => {
   const { email, firstname, lastname, password } = req.body;
